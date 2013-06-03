28801c248a180a749a799565491d04cf22b47ac4
nero <nero_step@icloud.com>
2013-06-03 22:30:01 +0800

    Initial Commit

---
+++ objective-c learn/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objective-c learn
+//
+//  Created by nero on 13-6-3.
+//  Copyright (c) 2013年 nero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
